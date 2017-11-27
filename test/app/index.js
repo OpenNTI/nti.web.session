d60a85e58d95201686e1b07014567e76725044b9
Jonathan Grimes <jsg2021@gmail.com>
2017-11-27 13:35:49 -0600

    initial commit

---
+++ test/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'nti-style-common/all.scss';
+
+ReactDOM.render(
+	React.createElement('div'),
+	document.getElementById('content')
+);
