8d9d20a3fe6b10c6d7cf4a41cb57a2d0ca613af5
Jonathan Grimes <jsg2021@gmail.com>
2017-11-28 18:42:58 -0600

    work in progress...

--- src/ViewEvent.jsx
+++ src/ViewEvent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { toAnalyticsPath } from 'nti-analytics';
 
 export default class ViewEvent extends React.Component {
 	static propTypes = {
@@ -12,8 +13,49 @@ export default class ViewEvent extends React.Component {
 		children: PropTypes.any,
 	}
 
-	static contextTypes: {
+	static contextTypes = {
 		analyticsManager: PropTypes.object.isRequired
+	}
+
+	getEvent ({type} = this.props) {
+		const {analyticsManager: manager} = this.context;
+		return manager && manager[type];
+	}
+
+	async getEventArgs ({resourceId, ...data} = this.props) {
+		delete data.children;
+
+		if (data.context) {
+			data.context = toAnalyticsPath(await Promise.resolve(data.context), resourceId);
+		}
+
+		return [resourceId, data];
+	}
+
+	async send (trigger) {
+		const event = this.getEvent();
+		const args = await this.getEventArgs();
+
+		if (event && !event[trigger] && trigger === 'start') {
+			trigger = 'send';
+		}
+
+		if (event && event[trigger]) {
+			event[trigger](...args);
+		}
+	}
+
+
+	componentDidMount () {
+		this.send('start');
+	}
+
+	componentDidUpdate () {
+		this.send('update');
+	}
+
+	componentWillUnmount () {
+		this.send('stop');
 	}
 
 	render () {
