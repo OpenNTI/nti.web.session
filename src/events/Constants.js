ccf7f67a3a3cbe8caa04450159b3c5520834511b
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-19 17:48:33 -0500

    add events for topics

--- src/events/Constants.js
+++ src/events/Constants.js
@@ -2,6 +2,10 @@ export const NOTE_CREATED = 'note-created';
 export const NOTE_UPDATED = 'note-updated';
 export const NOTE_DELETED = 'note-deleted';
 
+export const TOPIC_CREATED = 'topic-created';
+export const TOPIC_UPDATED = 'topic-updated';
+export const TOPIC_DELETED = 'topic-deleted';
+
 export const ASSIGNMENT_SUBMITTED = 'assignment-submitted';
 
 export const ASSESSMENT_SUBMITTED = 'assessment-submitted';
