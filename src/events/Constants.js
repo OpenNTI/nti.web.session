f42abea9cec0e793f2076f4431c9ee3710dac148
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-27 16:08:02 -0500

    Add events for topic comment creation/updates/deletion

--- src/events/Constants.js
+++ src/events/Constants.js
@@ -6,6 +6,10 @@ export const TOPIC_CREATED = 'topic-created';
 export const TOPIC_UPDATED = 'topic-updated';
 export const TOPIC_DELETED = 'topic-deleted';
 
+export const TOPIC_COMMENT_CREATED = 'topic-comment-created';
+export const TOPIC_COMMENT_UPDATED = 'topic-comment-updated';
+export const TOPIC_COMMENT_DELETED = 'topic-comment-deleted';
+
 export const ASSIGNMENT_SUBMITTED = 'assignment-submitted';
 
 export const ASSESSMENT_SUBMITTED = 'assessment-submitted';
