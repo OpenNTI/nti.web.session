bc27bf8a6fdaee1979e96139de39bcbaf0df3da0
Jonathan Grimes <jsg2021@gmail.com>
2020-12-18 22:01:28 -0600

    NTI-10052 - move bus to lib-commons

--- src/events/Bus.js
+++
@@ -1,5 +0,0 @@
-import EventEmitter from 'events';
-
-const Bus = new EventEmitter ();
-Bus.setMaxListeners(0);
-export default Bus;
--- src/events/index.js
+++ src/events/index.js
@@ -1,15 +1,15 @@
-import Bus from './Bus';
+import {Events} from '@nti/lib-commons';
 
 export * from './Constants';
 
 export function emit (name, ...args) {
-	return Bus.emit(name, ...args);
+	return Events.Bus.emit(name, ...args);
 }
 
 export function addListener (name, fn) {
-	return Bus.addListener(name, fn);
+	return Events.Bus.addListener(name, fn);
 }
 
 export function removeListener (name, fn) {
-	return Bus.removeListener(name, fn);
+	return Events.Bus.removeListener(name, fn);
 }
