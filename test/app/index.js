9e68bf89ebd3048c0cb50e7cbcb73d1275268496
Jonathan Grimes <jsg2021@gmail.com>
2021-04-13 11:33:23 -0500

    use storybook for component dev here

--- test/app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import '@nti/style-common/all.scss';
-
-ReactDOM.render(
-	React.createElement('div'),
-	document.getElementById('content')
-);
